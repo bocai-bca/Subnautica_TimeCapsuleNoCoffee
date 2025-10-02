2ee0fb34838ba102e2d3d3c0aece10102f0845eb
Bocai_BCA <105545253+bocai-bca@users.noreply.github.com>
2025-10-02 19:41:35 +0800

    Initial commit

---
+++ TimeCapsuleNoCoffee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace TimeCapsuleNoCoffee
+{
+	[BepInPlugin("net.bcasoft.timecapsulenocoffee", "TimeCapsuleNoCoffee", "1.0.0")]
+	[BepInProcess("Subnautica.exe")]
+	public class TimeCapsuleNoCoffee : BaseUnityPlugin
+	{
+		public void Awake()
+		{
+			Harmony harmony = new Harmony("net.bcasoft.timecapsulenocoffee");
+			harmony.PatchAll();
+			Logger.LogInfo("Loaded.");
+			LogSource = Logger;
+		}
+
+		public static ManualLogSource LogSource;
+	}
+
+	[HarmonyPatch(typeof(TimeCapsuleContentProvider), nameof(TimeCapsuleContentProvider.GetItems))]
+	public class Patch_TimeCapsuleContentProvider
+	{
+		static List<TimeCapsuleItem> PostFix(List<TimeCapsuleItem> items)
+		{
+			List<TimeCapsuleItem> result = new List<TimeCapsuleItem>();
+			foreach (TimeCapsuleItem item in items)
+			{
+				if (item.techType == TechType.Coffee)
+				{
+					TimeCapsuleNoCoffee.LogSource.LogInfo("Removed a coffee in timecapsule!");
+                    continue;
+				}
+				result.Add(item);
+				TimeCapsuleNoCoffee.LogSource.LogInfo("Foreached a item in timecapsule, techtype = " + item.techType.ToString() + "(" + ((int)item.techType) + ")");
+			}
+			return result;
+		}
+	}
+}
